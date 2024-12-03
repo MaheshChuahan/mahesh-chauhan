3401d1ab2b83c077795ea00f3fdfc8f88072e432
Mahesh Chauhan <mahesh12041156@gmail.com>
2024-12-04 01:08:12 +0530

    forloop 3/12/24
    forloop 3/12/24

---
+++ forloop.js
@@ -0,0 +1,72 @@
+// for loop 
+// let i;
+// for ( i = 1; i < 6; i++){
+//     console.log(i);
+// }
+// console.log("i ===>", i);
+
+
+//  1 
+
+// for (let i = 10; i >= 1; i--) {
+//     console.log(i);
+// }
+
+// 2
+
+// for (let i = 51; i <= 79; i++){
+//     console.log(i);
+// }
+
+// 3 
+
+// let n;
+// n = Number(prompt('enter your number'))
+// for (i = 1 ; i <= n ; i++) {
+//     console.log(i);
+//  }
+
+// 4
+
+// let n = Number(prompt('enter your number'));
+// for (i = -n; i <= 1 ; i++) {
+//     console.log(i);
+// }
+
+// 5
+
+// let n = Number(prompt('enter your number'));
+// for (i = -n; i <= n ; i++) {
+//     console.log(i);
+// }
+
+// 6
+
+// let n = Number(prompt('enter your number'));
+// for (i = 1; i <= n; i+=2) {
+//     console.log(i);
+// }
+
+// 7
+
+// let n = Number(prompt('enter your number'));
+// for (i = 0; i <= n; i+=2) {
+//     console.log(i);
+// }
+
+// 8
+// for (i = 1; i <= 5; i++) {
+//     if (i%2 == 0){
+//         console.log(i**2);
+//     }
+//     if (i%2 == 1){
+//         console.log(i);
+//     }
+// }
+// 9
+
+// for(i=0;i<=10;i++){
+//     if(i+=i){
+//         console.log(i)
+//     }
+// }
