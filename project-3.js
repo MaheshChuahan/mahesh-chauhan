a53cb6e0e8f32426607e43f231424b4fcc356be6
Mahesh Chauhan <mahesh12041156@gmail.com>
2024-12-09 16:49:10 +0530

    project-3  9/12/24

---
+++ project-3.js
@@ -0,0 +1,68 @@
+// 1  : Write JS Program to check number is Palindrome or not 
+
+// let n = Number(prompt('enter your number'));
+// let value = n;
+// let reversnumber = 0;
+// while (n > 0) {
+//     let number = n % 10;
+//     reversnumber = reversnumber * 10 + number;
+//     n = (n - (n % 10) ) / 10;
+// }
+// if (value === reversnumber) {
+//     console.log('palindrome number ',value)
+// }
+// else {
+//     console.log('not a palindrome number ',value)
+// }
+
+
+
+//  2  : Print Series 1, 4, 3, 16, 5, ....
+// for (i = 1; i <= 5; i++) {
+//     if (i % 2 == 0) {
+//         console.log(i ** 2);
+//     }
+//     if (i % 2 == 1) {
+//         console.log(i);
+//     }
+// }
+
+
+
+// 3 : Print Multiplication table of n
+// let i =1 ;
+// let n = Number(prompt('enter your number'));
+// while (i<=10){
+//     i++;
+//     console.log(n + "*" + i + "=", n * i);
+
+// }
+
+
+
+//  4 : find out sum of digit of given number
+// let n = Number(prompt("enter your number"));
+// let sum = 0;
+
+// for (let i = 1; i <= n; i++) {
+//     sum += i;
+//     console.log(sum);
+
+// }
+
+
+
+//  5 : print series 1, 1, 2, 3, 5, 8, ...
+
+// let a = 1, b = 1 , i = 0;
+// console.log(a);
+// console.log(b);
+// while (i <= 5) {
+//     let c = a + b;
+//     i++;
+//     console.log(c);
+//     a = b;
+//     b = c;
+// }
+
+
