def0b066403cf84702a40733b2c310b0fe3b6073
Mahesh Chauhan <mahesh12041156@gmail.com>
2024-12-12 18:45:20 +0530

    Project Day  12/12/24

---
+++ project-4.js
@@ -0,0 +1,117 @@
+ 
+
+//1Write a JAVASCRIPT Program to print a greeting message using with argument no return type function when call a function without argument at that time print message Good Morning.
+ 
+//  function greeting(){
+//     console.log("good morning....")
+//  }
+//  greeting()
+//$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
+//2 writeVASCRIPT Program to find Circle area (area pi*r*r) using return type with argument function.
+
+//  function circle(radius){
+//       const area = Math.PI * radius ** 2;
+//       return area;
+//   }
+  
+//   console.log("The Area of circle :=",circle(4));
+
+  // output : circle area :50.26548245743669
+//$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
+//3 write JAVASCRIPT Program to find Triangle area (area = (1*h)/2) using return type with argument function.
+
+  // function areatriangle(base,Height,triangle){
+  //   triangle = 1/2* base *Height;
+  //   console.log("the area of triangle is :",triangle);
+  // }
+  // console.log(areatriangle(5,5));
+
+  // output : triangle area : 12.5
+
+//$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
+//4. Write a JAVASCRIPT Program to find Rectangle area = 1 h using return type with argument function.
+
+// function rectangle(l, h) {
+//   var area = l * h
+//   return area;
+// }
+// console.log("Area of rectangle is", rectangle(10, 20));
+
+// output rectanglearea : 200
+ 
+//$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
+// 5. Write a JAVASCRIPT Program to find ans of ((b*b)(4*a*c))/(2*a) using return type with argument function.
+
+  
+// function xyz(a, b, c) {
+//     let ans = (b * b) * (4 * a * c) / (2 * a)
+//     return ans;
+// }
+// console.log("find ans:",xyz(12, 5, 8));
+
+// output : find ans:400
+//$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
+// 6. Write a JAVASCRIPT Program to find ans of (a*a) + (2*a*b)+(b*b) using return type with argument function
+
+//   function abc(a,b) {
+//     let ans=(a*a)+(2*a*b)+(b*b)
+//     return ans;
+// }
+// console.log("find ans:",abc(12,5));
+
+// output 289
+//$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
+// 7. Write a JAVASCRIPT Program to convert Fahrenheit to Celsius (c = (f-32)/1.8) using return type with argument function
+
+//  let f = 8;
+// function celsius(a) {
+//     let c = (f * 32) /1.8;
+//     return c;
+// }
+// console.log(celsius(f));
+
+// output 142.222
+//$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
+// 8. Write a JAVASCRIPT Program to convert Celsius to Fahrenheit (f= (c*1.8)+32) using return type with argument function
+
+  // let celsius = 10;
+// function fahaenheit(a) {
+//     let f = celsius * 1.8 + 32;
+//     return f;
+// }
+// console.log(fahaenheit(celsius));
+
+// output : 37
+//$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$4$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
+// 9. Write a JAVASCRIPT Program to find if a given number is odd or even using an argument with no return type.
+
+
+// let num = 1;
+// function number(odd) {
+//     if (odd % 2 == 0) {
+//         console.log("even number");
+//     }
+//     else {
+//         console.log("odd number");
+//     }
+// }
+
+// number(num);
+
+//   output : 1 is odd number
+
+//$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
+
+// 10. Write a JAVASCRIPT Program to swap a value without third variable using with
+
+//   function swapping(x, y) {
+//     console.log("Before swapping : x =", x, ", y =", y);
+//     x = x + y;
+//     y = x - y;
+//     x = x - y;
+
+//     console.log("After swapping : x =", x, ", y =", y);
+// }
+// swapping(10, 12);
+
+// output : Before swapping : x = 10 , y = 12  after swapping : x = 12 , y = 10
