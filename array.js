f494515452c8cd946df6cc9881b45d8f789935ef
Mahesh Chauhan <mahesh12041156@gmail.com>
2024-12-13 18:34:51 +0530

    Array With Functions Push ,Pop, shift, upshifts, Splice string function  13/12/24

---
+++ array.js
@@ -0,0 +1,16 @@
+// aray: store multiple value in single variable ( index start with ==  0)
+
+let arr=[11,22,33,44,55]
+console.log(arr);
+console.log(arr[3])
+
+// push method : insert into last index...
+
+// pop method : remove from last  index...
+
+// shift method : remove frome first index....
+
+// unshift method : insert into first index...
+
+// splice method : any position remove or added element....
+
