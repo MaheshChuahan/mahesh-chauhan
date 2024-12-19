78b861707d76a56896986719cbf45773583e2d66
Mahesh Chauhan <mahesh12041156@gmail.com>
2024-12-19 18:56:55 +0530

    JS Objects ( PPT) Object Property Object Methods   19/12/24

---
+++ object-1.js
@@ -0,0 +1,37 @@
+// object
+
+// object => everything is object 
+
+// object => collection of multiple datatype (physical entity)
+
+let person = {
+    name:"mahesh",
+    age:20,
+    emil:"mahesh12041156@gmail.com",
+    mobile:9664782785,
+}
+console.log(person);
+
+// Dot operstore
+
+console.log(person.name);
+
+console.log(person.emil);
+
+console.log(person.age);
+
+console.log(person.mobile);
+
+// ['key']
+
+console.log(person['name']);
+
+console.log(person['emil']);
+
+console.log(person['mobile']);
+
+console.log(person['age']);
+
+// object value
+
+
