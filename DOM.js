2d41b2610f21381bd9b45975c8343d05dc47ae4a
Mahesh Chauhan <mahesh12041156@gmail.com>
2024-12-26 19:08:36 +0530

    DOM 25/12/24

---
+++ DOM.js
@@ -0,0 +1,24 @@
+//  DOM = document object model
+
+// document.getElementById
+// document.getElementByTagName
+// document.getElementByClassName
+// queryselector & queryselectorAll
+
+// ID //
+
+// let h2 = document.getElementById('name')
+// console.log(h2)
+// h2.innerHTML = "javascript DOM"
+// h2.style.color = "red"
+// h2.setAttribute("class", "hello world");
+
+// TAG NAME //
+
+let data = document.getElementsByTagName('p');
+console.log("data");
+data.style.color="blue";
+
+// CLASS NAME //
+
+
