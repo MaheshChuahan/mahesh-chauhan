03a4b75f05ee103d4d006dc957671be946f93078
Mahesh Chauhan <mahesh12041156@gmail.com>
2025-01-01 18:47:02 +0530

    Class 31/12/2024

---
+++ class.js
@@ -0,0 +1,18 @@
+// class ==> logical antity of a object
+
+class Test{
+    sayHellow(){
+        console.log("hellow good byy 2024");
+    }
+
+    addition (x , y){
+        console.log("addition of x and y is ", x+y);
+    }
+    
+}
+let obj = new Test();
+    obj.sayHellow();
+
+    obj.addition(11,22);
+
+
